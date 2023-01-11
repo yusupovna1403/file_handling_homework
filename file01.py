f673ab66cec909f4a134d6c0fd76a595ae667336
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 21:19:06 +0500

    file_01

--- file01.py
+++ file01.py
@@ -6,5 +6,22 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    f = open(a,encoding = 'utf8')
+    data = f.read()
+    lst = data.split(',')
+    i = 0
+    list = []
+    for i in lst:
+        b = int(i)
+        list.append(b)
+    return list
 
+a = 'txt_file/data01.txt'
+print(main(a))
+
+
+
+
+
+    
 # Read data from file
