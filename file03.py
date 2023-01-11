7267fea1c9390eec8fc0d293fd1ae6b8c9f7ed91
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 21:19:40 +0500

    file_03

--- file03.py
+++ file03.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    file = open(a,encoding='utf8')
+    data = file.read()
+    list = []
+    i = 0
+    for i in data:
+        if i >= '0' and i <= '9':
+            list.append(i)
+    return list
+a = 'txt_file/data03.txt'
+print(main(a))
 # Read data from file
