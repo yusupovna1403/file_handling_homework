475014f9d3c8623189cf76fd3f0357d84e15e9c1
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 19:31:34 +0500

    file_02

--- file02.py
+++ file02.py
@@ -1,10 +1,22 @@
 def main(data:str):
     """
-    The data is from the file. Return number of characters in the file.
+    The data:str is from the file. Return number of characters in the file.
     Args:
         data: str
     Returns:
         int: return answer
     """
+    
+    file = open('txt_file/data02.txt','r')
+    data = file.read()
+    return len(data)
+    file.close()
+
+a = 'txt_file/data02.txt'
+print(main(a))
+
+    
+    
+    
 
 # Read data from file
