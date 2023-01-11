b20ddca410c1a614abec16ecd7fca410fd9a3df6
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 22:55:52 +0500

    file_10

--- file10.py
+++ file10.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    file = open(a,encoding='utf8')
+    data = file.readlines()
+    data = [data.rstrip() for data in data]
+    max = len(data[0])
+    i = 1
+    for i in data:
+        if len(i) > max:
+            max = len(i)
+    return max  
+a = 'txt_file/data10.txt'
+print(main(a))
 
 # Read data from file
