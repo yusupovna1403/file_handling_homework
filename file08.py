e98508ba9da8a52e8afc7b351afa01c632884f2d
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 23:03:55 +0500

    file_08

--- file08.py
+++ file08.py
@@ -6,5 +6,20 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    file = open(a,encoding='utf8')
+    data = file.read()
+    data = data.rstrip()
+    data = data.replace('\n','')
+    list = []
+    for i in data:
+        if i >= '0' and i <= '9':
+            list.append(i)
+    max = list[0]
+    i = 1
+    for i in list:
+        if i > max:
+            max = i
+    return max
+a = 'txt_file/data08.txt'
+print(main(a))
 # Read data from file
