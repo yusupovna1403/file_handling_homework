760a29f9ae22ea972490d4160af9d801f9b03272
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 22:21:41 +0500

    file_07

--- file07.py
+++ file07.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    file = open(a,encoding='utf8')
+    data = file.read()
+    data = data.rstrip()
+    data = data.replace('\n','')
+    sum = 0
+    for i in data:
+        if i >= '0' and i <= '9':
+            sum+=int(i)
+    return sum
+a = 'txt_file/data07.txt'
+print(main(a))
 # Read data from file
