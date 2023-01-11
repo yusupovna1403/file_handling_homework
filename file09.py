e9d975c8a1a8833fc13049bdb142b241d020dff7
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 23:04:25 +0500

    file_09

--- file09.py
+++ file09.py
@@ -6,5 +6,21 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    file = open(a,encoding='utf8')
+    data = file.read()
+    data = data.rstrip()
+    data = data.replace('\n','')
+    list = []
+    for i in data:
+        if i >= '0' and i <= '9':
+            list.append(i)
+    min = list[0]
+    i = 1
+    for i in list:
+        if i < min:
+            min = i
+    return min
+a = 'txt_file/data09.txt'
+print(main(a))
 
 # Read data from file
