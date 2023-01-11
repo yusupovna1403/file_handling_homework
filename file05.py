536c4c447282bf006210c019055a188f7cb39e45
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 21:34:14 +0500

    file_05

--- file05.py
+++ file05.py
@@ -6,5 +6,21 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    file = open(a,encoding='utf8')
+    data = file.read()
+    list = []
+    i = 0
+    num = 0
+    str = 0
+    data = data.rstrip()
+    for i in data:
+        if i >= '0' and i <= '9':
+            num+=1
+        else:
+            str+=1
+    list.append(num)
+    list.append(str)
+    return list
+a = 'txt_file/data05.txt'
+print(main(a))
 # Read data from file
