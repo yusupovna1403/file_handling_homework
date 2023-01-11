c5663a0703afa69ebe4a76b808867db464703f6e
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-11 21:24:17 +0500

    file_04

--- file04.py
+++ file04.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    file = open(a,encoding='utf8')
+    data = file.read()
+    list = []
+    i = 0
+    num = ['0','1','2','3','4','5','6','7','8','9']
+    for i in data:
+        if i not in num:
+            list.append(i)
+    return list
+a = 'txt_file/data04.txt'
+print(main(a))
     
 # Read data from file
